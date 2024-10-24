d2a622b2bf04e670f4d0f80c5550418900f1492f
haluongtan <160831939+haluongtan@users.noreply.github.com>
2024-10-24 16:10:14 +0700

    Update frmSuaNhanVien.cs

--- frmSuaNhanVien.cs
+++ frmSuaNhanVien.cs
@@ -1,9 +1,13 @@
-﻿using System;
+using BUS;
+using DAL.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,41 +16,59 @@ namespace QLNhanVien
 {
     public partial class frmSuaNhanVien : Form
     {
-        private frmMain formChinh; 
-        private Model1 dbContext;
+        private NhanVienBUS nhanVienBUS = new NhanVienBUS();
+        private int maNhanVien;
         private NhanVien nhanVien;
+        private frmMain formChinh;
 
-        public frmSuaNhanVien(frmMain form1, Model1 context, NhanVien nv)
+
+
+        public frmSuaNhanVien( frmMain formChinh,int maNhanVien)
         {
             InitializeComponent();
-            formChinh = form1;
-            dbContext = context;
-            nhanVien = nv;
+            this.maNhanVien = maNhanVien;
+            this.formChinh = formChinh;
             err = new ErrorProvider(); 
 
         }
 
         private void frmSuaNhanVien_Load(object sender, EventArgs e)
         {
-            txtMaNV.Text = nhanVien.MaNhanVien.ToString();
-            txtHoTen.Text = nhanVien.HoTen;
-            dtpNgaySinh.Value = nhanVien.NgaySinh ?? DateTime.Now;
-            if (nhanVien.GioiTinh == "Nam")
+            NhanVien nv = nhanVienBUS.LayNhanVienTheoMa(maNhanVien);
+            if (nv == null)
+            {
+                MessageBox.Show("Không tìm thấy nhân viên!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+                return;
+            }
+            nhanVien = nv;
+
+            txtMaNV.Text = nv.MaNhanVien.ToString();
+            txtHoTen.Text = nv.HoTen;
+            dtpNgaySinh.Value = nv.NgaySinh ?? DateTime.Now;
+            if (nv.GioiTinh == "Nam")
                 rbNam.Checked = true;
             else
                 rbNu.Checked = true;
-            txtSDT.Text = nhanVien.SDT;
-            txtDiaChi.Text = nhanVien.DiaChi;
-            txtMaPhongBan.Text = nhanVien.MaPhongBan?.ToString();
-            txtMaChucVu.Text = nhanVien.MaChucVu?.ToString();
-            dtpNgayVaoLam.Value = nhanVien.NgayVaoLam ?? DateTime.Now;
-            txtEmail.Text = nhanVien.Email;
+            txtSDT.Text = nv.SDT;
+            txtDiaChi.Text = nv.DiaChi;
+            txtMaPhongBan.Text = nv.MaPhongBan?.ToString();
+            txtMaChucVu.Text = nv.MaChucVu?.ToString();
+            dtpNgayVaoLam.Value = nv.NgayVaoLam ?? DateTime.Now;
+            txtEmail.Text = nv.Email;
+
+            if (!string.IsNullOrEmpty(nv.AvatarPath) && System.IO.File.Exists(nv.AvatarPath))
+            {
+                ptrAvatar.Image = Image.FromFile(nv.AvatarPath);
+                ptrAvatar.Tag = nv.AvatarPath; // Lưu đường dẫn vào Tag
+            }
         }
 
         private void btnXacNhan_Click(object sender, EventArgs e)
         {
             if (!ValidateInputs()) return;
 
+            // Cập nhật lại các thuộc tính của nhân viên
             nhanVien.HoTen = txtHoTen.Text;
             nhanVien.NgaySinh = dtpNgaySinh.Value;
             nhanVien.GioiTinh = rbNam.Checked ? "Nam" : "Nữ";
@@ -56,11 +78,13 @@ namespace QLNhanVien
             nhanVien.MaChucVu = int.TryParse(txtMaChucVu.Text, out int maChucVu) ? (int?)maChucVu : null;
             nhanVien.NgayVaoLam = dtpNgayVaoLam.Value;
             nhanVien.Email = txtEmail.Text;
+            nhanVien.AvatarPath = ptrAvatar.Tag?.ToString();
 
-            dbContext.SaveChanges();
+            nhanVienBUS.SuaNhanVien(nhanVien);
             MessageBox.Show("Cập nhật thông tin nhân viên thành công!");
 
-            formChinh.LoadData();
+            // Gọi hàm cập nhật dữ liệu trên DataGridView
+            formChinh.CapNhatNhanVien(nhanVien);
 
             this.Close();
         }
@@ -137,6 +161,24 @@ namespace QLNhanVien
             {
                 err.SetError(txtSDT, ""); 
             }
+        }
+
+        private void btnThemHinhAnh_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    ptrAvatar.Image = Image.FromFile(openFileDialog.FileName);
+                    ptrAvatar.Tag = openFileDialog.FileName; // Lưu đường dẫn ảnh vào Tag của PictureBox
+                }
+            }
+        }
+
+        private void ptrAvatar_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
